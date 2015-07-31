5abd52a5b8c271863e751f5a608950e76c428ee0
mgruesen <mgruesen7@gmail.com>
2015-07-31 12:48:16 -0400

    Moved pkt-gen script to top level; start script fix

--- start_vagrant.sh
+++ start_vagrant.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Set the vagrant box current working directory
-export VAGRANT_CWD=$PWD/dev-machine
-
 # Set the path to the freeflow repo on the host machine
 # and start the 'dut' guest machine with a shared folder
 # mounted at '/freeflow'.
-FF_PATH=$PWD vagrant up dut
+CURR=$PWD
+cd dev-machine
+FF_PATH=$CURR vagrant up dut
